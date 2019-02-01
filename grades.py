8d767504781ee29155f02ba1909f93859f3ed54e
Alex Haug <stahh02@moravian.edu>
2019-02-01 14:00:31 -0500

    Test and Implementation for dropping lowest grade(s)

--- grades.py
+++ grades.py
@@ -1,5 +1,7 @@
 
-def compute_hw_average(grades):
+def compute_hw_average(grades, numdrop):
     if len(grades) == 0:
         return 0
+    for x in range(numdrop):
+        grades.remove(min(grades))
     return sum(grades)/len(grades)
--- test_grades.py
+++ test_grades.py
@@ -4,14 +4,19 @@ from grades import compute_hw_average
 
 def test_zero_grades():
     grades = []
-    assert compute_hw_average(grades) == 0
+    assert compute_hw_average(grades, 0) == 0
 
 
 def test_single_grade():
     grades = [42]
-    assert compute_hw_average(grades) == 42
+    assert compute_hw_average(grades, 0) == 42
 
 
 def test_double_grades():
     grades = [18, 28]
-    assert compute_hw_average(grades) == 23
+    assert compute_hw_average(grades, 0) == 23
+
+
+def test_drop_lowest_grade_zeros():
+    grades = [10,20,30]
+    assert compute_hw_average(grades, 1) == 25
